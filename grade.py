b5368b0a80cfd9f2e8351423224c55827ffe0f2d
kantale <alexandros.kanterakis@gmail.com>
2021-03-13 12:05:01 +0200

    send mail on grade.py

--- grade.py
+++ grade.py
@@ -11,10 +11,69 @@ import os
 import glob
 import json
 import email
+import time
 import argparse
+import smtplib, ssl # For mail
+import pandas as pd
 
 from itertools import groupby
 from collections import defaultdict
+from os.path import expanduser
+
+def get_home_dir():
+    '''
+    '''
+    return expanduser("~")
+
+class Mail:
+
+    PASSWORD_PATH = '.gmail/settings.json'
+
+    def __init__(self,):
+        self.connect_to_gmail()
+
+    @staticmethod
+    def get_password():
+        password_filename = os.path.join(get_home_dir(), Mail.PASSWORD_PATH)
+        with open(password_filename) as f:
+            data = json.load(f)
+        return data['password']
+
+    def connect_to_gmail(self,):
+        port = 587  # For starttls
+        smtp_server = "smtp.gmail.com"
+        sender_email = "alexandros.kanterakis@gmail.com"
+        password = Mail.get_password() 
+
+        context = ssl.create_default_context()
+        self.server = smtplib.SMTP(smtp_server, port)
+
+        self.server.ehlo()  # Can be omitted
+        self.server.starttls(context=context)
+        self.server.ehlo()  # Can be omitted
+        self.server.login(sender_email, password)
+
+        print ('CONNECTED TO GMAIL')
+
+
+
+    def do_send_mail(self, to, subject, text, sleep=10, actually_send_mail=False):
+
+        sender_email = "alexandros.kanterakis@gmail.com"
+        receiver_email = to
+        message = 'Subject: {}\n\n{}'.format(subject, text)
+
+
+        if actually_send_mail:
+            self.server.sendmail(sender_email, receiver_email, message.encode("utf8")) # msg.encode("utf8")
+        time.sleep(sleep)
+        print ('Mail sent')
+
+    def disconnect_from_gmail(self,):
+        self.server.quit()
+        print ('DISCONNECTED FROM GMAIL')
+
+
 
 class Grades:
 
@@ -30,20 +89,82 @@ class Grades:
         'Απαντηση ασκησης', 'Απάντηση ασκησης', 'απαντηση ασκησης',
         'Task_', 'απαντηση ακσησης', 'απάντηση άσκησης',
         'this is the solution for ex.', r'-+ΑΣΚΗΣΗ',
-        "'Ασκηση", "Αskisi", "Άσκση",
+        "'Ασκηση", "Αskisi", "Άσκση", "asksisi"
     ]
 
     ex_regexp = re.compile(r'^\s*#+\s*({})\s*(?P<ask>\d+)'.format('|'.join(declarations)))
 
     SOLUTIONS_FILENAME_PATTERN = 'AM_{id_}_ASK_{ASK}'
 
-    def __init__(self, directory, solutions_dir, action, ex=None):
+    MAIL_PATTERN = '''
+Γεια σας,
+
+Παρακάτω ακολουθεί η βαθμολογία σας στις ασκήσεις 1-20 στο μάθημα:
+ΒΙΟΛ-494 - Εισαγωγή στον προγραμματισμό
+
+AM: {AM}
+
+
+{EXERCISES}
+
+================================
+Συγκεντρωτικά:
+
+{SUMMARY}
+
+Για απορίες στείλτε DM στο slack!
+
+Χαιρετώ,
+Αλέξανδρος Καντεράκης
+
+'''
+
+    MAIL_EXERCISE_PATTERN = '''
+================================
+  Άσκηση: {EXERCISE}
+================================
+  Η λύση σας:
+--------------------------------
+{SOLUTION}
+--------------------------------
+Σχόλια:
+{COMMENT}
+--------------------------------
+Βαθμός: {GRADE}/10
+--------------------------------
+'''
+
+    MAIL_SUBJECT = 'ΒΙΟΛ-494 - Βαθμοί ασκήσεων 1-20'
+
+    def __init__(self, directory, solutions_dir, action, 
+            ex=None, 
+            actually_send_mail=False,
+            start = 1,
+            end = 20,
+
+        ):
         self.dir = directory
         self.solutions_dir = solutions_dir
+        self.actually_send_mail = actually_send_mail
+        self.start = start
+        self.end = end
+        self.exercises_range = list(range(self.start, self.end+1))
+
         self.get_filenames(ex)
         self.get_all_exercises()
+
         if action == 'grade':
             self.grade()
+        elif action == 'send_mail':
+            self.collect_all_grades()
+
+            self.mail = Mail()
+            self.send_mail()
+            self.mail.disconnect_from_gmail()
+        else:
+            raise Exception('Unknown action: {}'.format(action))
+
+
 
     def get_solutions_filename(self, id_, exercise):
 
@@ -51,12 +172,22 @@ class Grades:
 
         return os.path.join(self.solutions_dir, filename)
 
-    def get_grade_from_comment(self, comment):
+    def get_grade_from_comment(self, comment=None, filename=None):
+
+        if filename:
+            with open(filename) as f:
+                comment = f.read()
+
         grades = [int(x) for x in comment.split('\n') if re.match(r'^\d+$', x)]
         assert len(grades) == 1
         assert grades[0] in range(0,11)
 
         return grades[0]
+
+    def remove_grade_from_comment(self, comment):
+
+        return '\n'.join(x for x in comment.split('\n') if not re.match(r'^\d+$', x))
+
 
 
     def grade(self,):
@@ -134,7 +265,107 @@ class Grades:
 
         # Group and sort according to ask / AM
         self.all_exercises = sorted((int(k2), k, v2) for k,v in self.all_exercises.items() for k2,v2 in v.items())
-        
+
+    def collect_all_grades(self,):
+        all_answers = {}
+
+        for ask, AM, answer in self.all_exercises:
+            if not AM in all_answers:
+                all_answers[AM] = {}
+
+            assert not ask in all_answers[AM]
+
+            filename = self.get_solutions_filename(AM, ask)
+            with open(filename) as f:
+                comment = f.read()
+
+            grade = self.get_grade_from_comment(comment)
+            comment = self.remove_grade_from_comment(comment)
+            
+
+            all_answers[AM][ask] = {
+                'answer': answer,
+                'grade': grade,
+                'comment': comment,
+            }
+        self.all_answers = all_answers
+
+    @staticmethod
+    def create_mail_address(AM):
+        if '@' in AM:
+            return AM
+
+        return AM + '@edu.biology.uoc.gr'
+
+    def send_mail(self,):
+
+        total = len(self.all_answers)
+        for i, AM in enumerate(self.all_answers):
+
+            mail_address = Grades.create_mail_address(AM)
+            #mail_address = 'alexandros.kanterakis@gmail.com'
+            print ('{}/{} -- {}'.format((i+1), total, mail_address))
+
+            mail = self.create_mail(AM)
+            #print(mail)
+
+            if True:
+                self.mail.do_send_mail(
+                    to=mail_address, 
+                    subject=self.MAIL_SUBJECT, 
+                    text=mail,
+                    actually_send_mail=self.actually_send_mail,
+                )
+            #a=1/0
+
+            
+
+
+    def create_exercise_mail(self,exercise, solution, comment, grade):
+
+        return self.MAIL_EXERCISE_PATTERN.format(
+            EXERCISE=exercise,
+            SOLUTION=solution,
+            COMMENT=comment,
+            GRADE=grade,
+        )
+
+    def create_mail(self, AM):
+
+        exercises_mail = ''
+
+        pandas_df = []
+
+        #for ASK, details in self.all_answers[AM].items():
+        for ASK in self.exercises_range:
+
+            if ASK in self.all_answers[AM]:
+                details = self.all_answers[AM][ASK]
+
+                answer = details['answer']
+                comment = details['comment']
+                grade = details['grade']
+            else:
+                answer = '\n'
+                comment = 'Δεν έστειλες τίποτα για αυτή την άσκηση!'
+                grade = 0
+
+
+            exercises_mail += self.create_exercise_mail(ASK, answer, comment, grade)
+            pandas_df.append({'Άσκηση': ASK, 'Βαθμός': grade})
+
+        pandas_df = pd.DataFrame(pandas_df)
+        summary = pandas_df.to_string(index=False)
+        summary += '\n\nΜέσος Όρος: {}'.format(pandas_df['Βαθμός'].mean())
+
+
+        ret = self.MAIL_PATTERN.format(
+            AM=AM,
+            EXERCISES=exercises_mail,
+            SUMMARY=summary,
+        )
+
+        return ret
 
     def get_type(self, filename):
         '''
@@ -237,6 +468,8 @@ class Grades:
 if __name__ == '__main__':
     '''
     python grade.py --dir /Users/admin/biol-494/exercises/ --sol /Users/admin/biol-494/solutions --action grade
+    python grade.py --dir /Users/admin/biol-494/exercises/ --sol /Users/admin/biol-494/solutions --action send_mail
+    python grade.py --dir /Users/admin/biol-494/exercises/ --sol /Users/admin/biol-494/solutions --action send_mail --actually_send_mail
     python grade.py --dir /Users/admin/biol-494/exercises/ --ex 3158 
     '''
 
@@ -245,7 +478,11 @@ if __name__ == '__main__':
     parser.add_argument("--sol", help="Directory with solutions")
     parser.add_argument("--ex", help="Examine only given exercise")
     parser.add_argument("--action", help="What to do: grade")
+    parser.add_argument("--actually_send_mail", action="store_true")
     args = parser.parse_args()
 
-    g = Grades(directory=args.dir, ex=args.ex, solutions_dir=args.sol, action=args.action)
+    g = Grades(directory=args.dir, ex=args.ex, solutions_dir=args.sol, 
+        action=args.action,
+        actually_send_mail=args.actually_send_mail,
+        )
     
