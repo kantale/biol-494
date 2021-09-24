b375124d0a374d3d96abea249dc3b627a56e4c80
kantale <alexandros.kanterakis@gmail.com>
2021-09-24 09:30:15 +0300

    added get_ask_sep.py

---
+++ get_ask_sep.py
@@ -0,0 +1,22 @@
+import random
+
+def get_ask(am):
+    salt = '59BCE074BA1E'
+    random.seed(str(am).strip()+salt)
+
+    groups = (
+        (1,  10),
+        (11, 20),
+        (21, 30),
+        (31, 40),
+        (41, 50),
+        (51, 60),
+        (61, 70),
+        (71, 80),
+        (81, 90),
+        (91, 100),
+    )
+
+    ret = [random.randint(g[0], g[1]) for g in groups]
+    return ret
+
